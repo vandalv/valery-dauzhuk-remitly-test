dbc85d79f87c42b77b9366f37cfad02b5001e027
vandalv <64989065+vandalv@users.noreply.github.com>
2023-03-03 13:48:09 +0100

    refactor and fixing imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from "react-dom/client";
-import CurrencyConverter from './CurrencyConverter';
+import CurrencyConverter from './components/CurrencyConverter';
 
 const ref = document.getElementById('root');
 const root = ReactDOM.createRoot(ref);
